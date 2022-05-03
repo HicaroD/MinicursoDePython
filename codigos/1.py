51576bb808699e9c835d473f920089c5c27f1e29
HicaroD <hdanrlley1@gmail.com>
2022-05-03 20:31:34 -0300

    Atualizando exemplos

--- codigos/1.py
+++ codigos/1.py
@@ -1,1 +1,1 @@
-print("Olá, mundo!")
+print("Olá, mundo")
--- codigos/2.py
+++ codigos/2.py
@@ -4,4 +4,3 @@ preco = 1.50
 pode_votar = False
 nomes = ["Leandro", "Juquinha", "Julia", "Rebeca"]
 alguma_coisa = None
-print(type(pode_votar))
