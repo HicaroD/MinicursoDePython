3b1a1ed3cc6b4e3db17acb1c74aafb9e69fddb4e
HicaroD <hdanrlley1@gmail.com>
2022-05-06 19:24:20 -0300

    adicionando exercício 38 sobre conjuntos

--- exercicios_resolvidos/conjuntos/38.py
+++ exercicios_resolvidos/conjuntos/38.py
@@ -1,3 +1,18 @@
 """
 Verifique se dois conjuntos têm, pelo menos, um elemento em comum.
 """
+
+c1 = {1, 2, 3, 4, 5}
+c2 = {4, 5, 6, 7}
+
+for elemento1 in c1:
+    for elemento2 in c2:
+        if elemento1 == elemento2:
+            print(str(elemento1) + " está contido em ambos conjuntos")
+
+for elemento1 in c1:
+    if elemento1 in c2:
+        print(str(elemento1) + " está contido em ambos conjuntos")
+
+if len(c1.intersection(c2)) > 0:
+    print("Temos elementos em comum")
