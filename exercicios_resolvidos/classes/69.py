2104f9045b701d47369266a1843ca226fa397543
HicaroD <hdanrlley1@gmail.com>
2022-05-07 17:44:50 -0300

    adicionando exercícios de classes

--- exercicios_resolvidos/classes/69.py
+++ exercicios_resolvidos/classes/69.py
@@ -8,3 +8,16 @@ construtores e métodos:
     b) Obter densidade demografica - Método que retorna a densidade populacional
        calculada através das propriedades do país.
 """
+
+class Pais:
+    def __init__(self, nome, nome_da_capital, dimensao, populacao):
+        self.nome = nome
+        self.nome_da_capital = nome_da_capital
+        self.dimensao = dimensao
+        self.populacao = populacao
+
+    def densidade_demografica(self):
+        return self.populacao / self.dimensao
+
+brasil = Pais("Brasil", "Brasilia", 8514876, 213000000)
+print(brasil.densidade_demografica())
--- exercicios_resolvidos/classes/70.py
+++ exercicios_resolvidos/classes/70.py
@@ -11,9 +11,34 @@ comportamentos:
        Perímetro: Soma de todos os lados
 
     b) ObterTipo – Retorna o tipo do triângulo: Equilátero, Isósceles ou Escaleno
-    
+
        Equilátero: Todos os lados iguais
        Isósceles: 2 lados iguais
        Escaleno:  Todos os lados diferentes
-
 """
+
+class Triangulo:
+    def __init__(self, a, b, c):
+        self.a = a
+        self.b = b
+        self.c = c
+
+    def ObterPerimetro(self):
+        return self.a + self.b + self.c
+    
+    def ObterTipo(self):
+        if self.a == self.b == self.c:
+            print('Equilátero')
+        elif (self.a == self.b) or (self.a == self.c) or (self.b == self.c):
+            print('Isósceles')
+        else:
+            print('Escaleno')
+
+
+a = int(input("Qual o valor do lado a?"))
+b = int(input("Qual o valor do lado b?"))
+c = int(input("Qual o valor do lado c?"))
+
+t = Triangulo(a, b, c)
+print(t.ObterPerimetro())
+t.ObterTipo()
